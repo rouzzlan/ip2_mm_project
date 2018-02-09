46aa6dfb6c657192779ec94f2c5766485e1ebd48
Oli <olivier.b@telenet.be>
2018-02-09 11:32:01 +0100

    Basis Login werkt, moeten nog manueel via Datagrip aan account toevoegen want we seeden nog geen accounts

--- src/main/java/be/kdg/musicmaker/repository/UserRepository.java
+++ src/main/java/be/kdg/musicmaker/repository/UserRepository.java
@@ -2,8 +2,12 @@ package be.kdg.musicmaker.repository;
 
 import be.kdg.musicmaker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("select u from User u where u.email = ?1")
+    User findByEmail(String email);
 }
