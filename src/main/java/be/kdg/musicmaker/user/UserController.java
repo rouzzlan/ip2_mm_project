8bcb36c2779e7e898d6ab6b1afe3227bfcca63e0
Unknown <unknown@example.com>
2018-02-16 17:22:14 +0100

    @CrossOrigins

--- src/main/java/be/kdg/musicmaker/user/UserController.java
+++ src/main/java/be/kdg/musicmaker/user/UserController.java
@@ -5,13 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class UserController {
     @Autowired
