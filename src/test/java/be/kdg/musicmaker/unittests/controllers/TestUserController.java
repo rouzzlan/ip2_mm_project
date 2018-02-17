93d3e4a835ab8657e6eda12fcb6d6902499dab3a
sam <samvdr@gmail.com>
2018-02-17 15:39:12 +0100

    getUsersJsonAnonymous toegevoegd

---
+++ src/test/java/be/kdg/musicmaker/unittests/controllers/TestUserController.java
@@ -0,0 +1,41 @@
+package be.kdg.musicmaker.unittests.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestUserController {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getUsersJsonAnonymous() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/getusersJson")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+}
+
+//public class UserController {
+//
+//    @Test
+//    public void getUsersJsonLeerling() throws Exception {
+//        String token = mockMvc.perform(MockMvcRequestBuilders.post("/oauth/token")
+//        .accept(MediaType.APPLICATION_JSON)
+//        .);
+//
+//        mockMvc.perform(MockMvcRequestBuilders.post("/getusersJson").accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isUnauthorized());
+//    }
+//}
