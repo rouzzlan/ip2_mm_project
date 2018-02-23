1ff59e0d3254fb9f6818e4a07cd5317a3e6fab30
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2018-02-23 16:07:25 +0100

    verdere uitwerking

--- src/test/java/be/kdg/musicmaker/fileTransfer/FileDownloadTest.java
+++ src/test/java/be/kdg/musicmaker/fileTransfer/FileDownloadTest.java
@@ -4,6 +4,7 @@ import be.kdg.musicmaker.MMAplication;
 import be.kdg.musicmaker.security.CorsFilter;
 import be.kdg.musicmaker.user.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
@@ -21,23 +22,23 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.Assert;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.io.IOException;
+import java.io.File;
 import java.util.LinkedList;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 @SpringBootTest(classes = MMAplication.class)
-public class FileUploadTest {
+public class FileDownloadTest {
     private static String ACCESS_TOKEN_Admin = "";
     private ObjectMapper objectMapper = new ObjectMapper();
 
@@ -68,15 +69,27 @@ public class FileUploadTest {
 
 
     @Test
-    public void testFileDownload() throws IOException {
-        try {
-            this.mockMvc.perform(get("/music_library/get_sample_file").header("Authorization", "Bearer " + ACCESS_TOKEN_Admin))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentType("audio/mpeg"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void testFileDownload() throws Exception {
+
+        this.mockMvc.perform(get("/music_library/get_sample_file").header("Authorization", "Bearer " + ACCESS_TOKEN_Admin))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("audio/mpeg"));
+
+    }
+
+    @Test
+    public void testDownloadFileContent() throws Exception {
+
+        ResultActions result = mockMvc.perform(get("/music_library/get_sample_file").header("Authorization", "Bearer " + ACCESS_TOKEN_Admin))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("audio/mpeg"));
+        byte[] byteArray = result.andReturn().getResponse().getContentAsByteArray();
+        String fileName = result.andReturn().getRequest().getHeader("filename");
+        File tempFile = testFolder.newFile("testfile.MP3");
+        FileUtils.writeByteArrayToFile(tempFile, byteArray);
+
+        Assert.notNull(tempFile);
+
     }
 
     private String obtainAccessToken(String username, String password) throws Exception {
