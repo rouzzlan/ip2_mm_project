35ef7034df458ed5549ffce149480ab016bb4aa8
sam <samvdr@gmail.com>
2018-03-08 20:03:29 +0100

    tijdelijke code toegevoegd waardoor alle testen slagen, sam werkt dit later af

--- src/test/java/be/kdg/musicmaker/lesson/TestLesson.java
+++ src/test/java/be/kdg/musicmaker/lesson/TestLesson.java
@@ -1,13 +1,70 @@
 package be.kdg.musicmaker.lesson;
 
 import be.kdg.musicmaker.MMAplication;
+import be.kdg.musicmaker.security.CorsFilter;
+import be.kdg.musicmaker.user.SeedData;
+import be.kdg.musicmaker.util.TokenGetter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * tijdelijke code die ervoor zorgt dat alle testen slagen
+ *
+ * sam werkt dit nog af!
+ */
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 @SpringBootTest(classes = MMAplication.class)
 public class TestLesson {
+    private static final Logger LOG = LoggerFactory.getLogger(SeedData.class);
+    private static String ACCESS_TOKEN_Admin = "";
+    private static String ACCESS_TOKEN_Student = "";
+    private static String ACCESS_TOKEN_Teacher = "";
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    LessonService lessonService;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private CorsFilter corsFilter;
+
+    private MockMvc mockMvc;
+    private TokenGetter tokenGetter;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+                .apply(springSecurity())
+                .addFilter(corsFilter).build();
+        tokenGetter = TokenGetter.getInstance(mockMvc);
+
+        ACCESS_TOKEN_Admin = tokenGetter.obtainAccessToken("user3@user.com", "user3");
+        ACCESS_TOKEN_Student = tokenGetter.obtainAccessToken("user@user.com", "user");
+        ACCESS_TOKEN_Teacher = tokenGetter.obtainAccessToken("user2@user.com", "user2");
+        LOG.info("the student's access token is: " + ACCESS_TOKEN_Student);
+        LOG.info("the teacher's access token is: " + ACCESS_TOKEN_Teacher);
+        LOG.info("the admin's access token is: " + ACCESS_TOKEN_Admin);
+    }
+
+    @Test
+    public void getLessonTypesAsAdmin() throws Exception {
+        mockMvc.perform(get("/lesson/types/").header("Authorization", "Bearer " + ACCESS_TOKEN_Admin))
+                .andExpect(status().isOk());
+    }
 }
